ed9be04b5c9b52a734225f045c017bfe2ee16d58
sandrincai <873217631@qq.com>
2020-06-20 22:11:01 +0800

    feature: 新增地图/走马灯组件

---
+++ src/components/BikeMap.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="amap-wrapper">
+    <el-amap
+      class="amap-box"
+      :vid="'amap-vue'"
+      :zoom="mapSettings.zoom"
+      :center="mapSettings.center"
+    >
+      <el-amap-marker
+        v-for="(maker, index) in mapMarkers"
+        :key="index"
+        :position="maker"></el-amap-marker>
+    </el-amap>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mapSettings: {
+          zoom: 15,
+          center: [113.944798, 22.530929]
+
+        },
+        mapMarkers: [
+          [113.945229, 22.530137],
+          [113.951059, 22.533475],
+          [113.949227, 22.536404],
+          [113.94911, 22.539225],
+          [113.947637, 22.540218],
+          [113.954149, 22.540718],
+          [113.955353, 22.544958],
+          [113.955892, 22.530988],
+          [113.944798, 22.530929],
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .amap-wrapper {
+    width: 100%;
+    height: 30vw;
+  }
+</style>
---
+++ src/components/HomeCarousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-carousel height="42vw">
+    <el-carousel-item v-for="(src, index) in homeImages" :key="index">
+      <el-image
+        :src="src"
+        fit="fill"
+        style="height:42vw;width: 100%"
+        :preview-src-list="homeImages"
+      ></el-image>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+  export default {
+    name: "HomeCarousel",
+    data() {
+      return {
+        homeImages: [
+          "static/img/components/home/2.jpg",
+          "static/img/components/home/3.jpg",
+          "static/img/components/home/4.jpg",
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
