74851e69513015a6ac6837954c53dfb129422675
sandrincai <873217631@qq.com>
2020-06-22 20:19:53 +0800

    minor: 宽度自适应窗口

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    min-width: 1600px;
   }
   html,body,#app{
     /*设置满屏，并且取消边框margin*/
