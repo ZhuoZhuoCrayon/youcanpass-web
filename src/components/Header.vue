7b6ae1a272b7dba79e089e6ec710adc88915ac3f
sandrincai <873217631@qq.com>
2020-06-22 20:17:51 +0800

    bugfix: 页面刷新导航重置问题

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <el-menu :default-active="activeIndex" :router="true" @select="handleSelect" class="el-menu-demo" mode="horizontal">
+    <el-menu
+      :default-active="activeIndex"
+      :router="true"
+      @select="handleSelect"
+      class="el-menu-demo"
+      mode="horizontal">
       <div class="re-login" v-if="!user.isLogin">
         <el-divider direction="vertical"/>
-        <el-button @click="gotoLogin" size="small" type="primary">登录</el-button>
-        <el-button @click="register" size="small">注册</el-button>
+        <el-button @click="gotoLogin" size="small" type="primary">Login</el-button>
+        <el-button @click="register" size="small">Register</el-button>
       </div>
       <el-submenu index="/user" v-else>
         <template slot="title">{{ user.name }}</template>
-        <el-menu-item @click="gotoInfo">个人信息</el-menu-item>
-        <el-menu-item index="/logout">注销</el-menu-item>
+        <el-menu-item @click="gotoInfo">Info</el-menu-item>
+        <el-menu-item index="/logout">Logout</el-menu-item>
       </el-submenu>
-      <el-menu-item index="/admin">管理员平台</el-menu-item>
-      <el-menu-item index="/teacher">讲师平台</el-menu-item>
-      <el-menu-item index="/student">学生平台</el-menu-item>
-      <el-menu-item index="/home">主页</el-menu-item>
+      <el-menu-item index="/guides">Guides</el-menu-item>
+      <el-menu-item index="/cost">Cost</el-menu-item>
+      <el-menu-item index="/reviews">Reviews</el-menu-item>
+      <el-menu-item index="/home">Home</el-menu-item>
     </el-menu>
 
     <el-dialog
@@ -25,19 +30,19 @@
       center>
       <el-form id="login-form" ref="form" :model="form" style="margin-bottom: 40px; margin-top: 20px">
         <el-form-item>
-          <el-input v-model="form.username" placeholder="学号"></el-input>
+          <el-input v-model="form.username" placeholder="username"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input v-model="form.password" placeholder="密码" show-password></el-input>
+          <el-input v-model="form.password" placeholder="password" show-password></el-input>
         </el-form-item>
         <el-form-item style="margin-top: 50px;margin-bottom: 0px">
           <el-button style="width: 100%;min-height: 40px"
                      type="primary"
-                     @click="login">提交</el-button>
+                     @click="login">Submit</el-button>
         </el-form-item>
         <el-form-item style="display: inline-block;float: right;">
           <el-button style="width: 100%" type="text"
-                     @click="register">还没账号？立即注册</el-button>
+                     @click="register">No account yet? Register now</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -52,11 +57,11 @@
         activeIndex2: '/home',
         loginDialogVisible: false,
         user: {
-          name: '蔡晓鑫',
+          name: 'Caixiaoxin',
           id: '1',
           isLogin: false,
           username : '2017151019',
-          password : '07124217'
+          password : '123'
         },
         form: {
           username: null,
@@ -64,7 +69,17 @@
         }
       };
     },
+    watch: {
+      $route() {
+        this.setCurrentRoute();
+      }
+    },
     methods: {
+      setCurrentRoute() {
+        console.log(this.$route);
+        this.activeIndex = this.$route.path;
+        // this.activeIndex= this.$route.path; // 通过他就可以监听到当前路由状态并激活当前菜单
+      },
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
       },
@@ -81,13 +96,16 @@
       },
       login(){
         if(this.user.username === this.form.username && this.user.password === this.form.password){
-          this.$message.success('登录成功');
+          this.$message.success('Success');
           this.user.isLogin = true;
           this.loginDialogVisible = false;
         }else{
-          this.$message.error('账户或密码错误');
+          this.$message.error('Error');
         }
       }
+    },
+    created() {
+      this.setCurrentRoute();
     }
   }
 </script>
